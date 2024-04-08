ed8c4824791a5509c511448afdd6208ae1049a9f
DagmaraSobczak <daga.sobczak@gmail.com>
2024-04-08 23:59:01 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './stylesheet/vars.css'
 import './stylesheet/common.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/wterapii">
+    <BrowserRouter basename="wterapii">
       <App />
     </BrowserRouter>
   </React.StrictMode>
